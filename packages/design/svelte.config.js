5c82cce567cd21b4f5ca81df6df96c257e670741
Vu1nea <284370798+Vu1nea@users.noreply.github.com>
2026-08-11 23:50:28 -0400

    transferred to mushpoint design

--- packages/design/svelte.config.js
+++
@@ -1,9 +0,0 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('svelte/compiler').CompileOptions} */
-export default {
-	preprocess: vitePreprocess(),
-	compilerOptions: {
-		runes: true
-	}
-};
